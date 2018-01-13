466cd44f601fe8f3fd9c281b0e86a244529fb898
egaunder <egaunder@gmail.com>
2018-01-13 08:45:50 -0800

    Add crud test validations for User model

--- server/src/models/test/user.test.js
+++ server/src/models/test/user.test.js
@@ -1,11 +1,11 @@
 import User from '../User';
-import initializeTestDb from '../../utils/test_helper';
+import { initializeTestDb, clearTestDb } from '../../utils/test_helper';
 
 describe('User', () => {
   let joe, jane, jack;
-  beforeAll(() => {
-    return initializeTestDb();
-  });
+  beforeAll(() => initializeTestDb());
+
+  afterAll(() => clearTestDb());
 
   beforeEach(done => {
 
@@ -31,13 +31,76 @@ describe('User', () => {
       .then(() => done())
   });
   
-  test('should be invalid if username is empty', (done) => {
-    var user = new User();
+  test('should be invalid if username is empty', done => {
+    const user = new User();
 
     user.validate((err) => {
-      expect(err.errors.username).toBeTruthy();
+      expect(err.errors.username).toBeDefined();
       done();
     });
   });
 
+  test('should be invalid if email is empty', done => {
+    const user = new User();
+
+    user.validate((err) => {
+      expect(err.errors.email).toBeDefined();
+      done();
+    });
+  });
+
+  test('should be invalid if password_hash is empty', done => {
+    const user = new User();
+
+    user.validate((err) => {
+      expect(err.errors.password_hash).toBeDefined();
+      done();
+    });
+  });
+
+  test('should insert a new user', done => {
+    const bob = new User({
+      username: 'bob',
+      email: 'bob@email.com',
+      password_hash: 'jfksjdlfjlsdkfuiewjril'
+    });
+
+    bob.save()
+      .then(user => {
+        expect(user.username).toBe("bob");
+        done();
+      });
+  });
+
+  test('should find a user', done => {
+    User.findOne({
+      username: 'JoeyChesnut'
+    })
+    .then(user => {
+      expect(user.username).toBe("JoeyChesnut");
+      done()
+    });
+  });
+
+  test('should update a user', done => {
+    const user = User.findOne({ username: "JoeyChesnut"})
+      .then(user => {
+        user.username = "JoeDimagion";
+        user.save()
+          .then(user => {
+            expect(user.username).toBe("JoeDimagion");
+            done()
+          });
+      });
+  });
+
+  test('should remove a user', done => {
+    User.findOneAndRemove({ username: "JoeDimagion"})
+    User.findOne({ username: "JoeDimagion"})
+      .then(user => {
+        expect(user).toBe(null);
+                
+      });
+    done();
+  });
 });
--- server/src/utils/test_helper.js
+++ server/src/utils/test_helper.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose';
 import keys from '../config/keys';
 
-function initializeTestDb() {
+export function initializeTestDb() {
   
   try {
     mongoose.connect(keys.mongoUriTest, { useMongoClient: true });
+    return new Promise(res => res(true));
   } catch (err) {
-    return new Promise((res, rej) => res(false));
+    return new Promise(res => res(false));
   }
-  return new Promise((res, rej) => res(true));
 }
 
-export default initializeTestDb;
+
+export function clearTestDb() {
+  try {
+    mongoose.connection.db.dropDatabase();
+    return new Promise(res => res(true))
+  } catch(err) {
+    return new Promise(res => res(false))
+  }
+}
